db11f8bdf0fc320f865cb8a7345a438eab9d882e
xuwei-k <6b656e6a69@gmail.com>
2014-04-12 17:31:28 +0900

    support more http methods

--- scalaj/src/main/scala/ScalajHttp.scala
+++ scalaj/src/main/scala/ScalajHttp.scala
@@ -10,11 +10,14 @@ object ScalajHttp{
 
   def apply(req: httpz.Request): Http.Request = {
     val r0 = req.method match {
-      case "GET"      => Http.get(req.url)
-      case "POST"     => req.body match {
+      case "GET" =>
+        Http.get(req.url)
+      case "POST" => req.body match {
         case None        => Http.post(req.url)
         case Some(bytes) => Http.postData(req.url, bytes)
       }
+      case other =>
+        Http(req.url).method(other)
     }
     val r1 = r0.params(req.params).headers(req.headers).options(OPTIONS)
     req.basicAuth.fold(r1){case (user, pass) => r1.auth(user, pass)}
