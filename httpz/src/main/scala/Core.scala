1fc128d275bd677f3bf7606b00f01121d3f285b4
xuwei-k <6b656e6a69@gmail.com>
2014-06-21 02:33:16 +0900

    Inject typeclass
    - http://d.hatena.ne.jp/xuwei/20140618/1403054751
    - https://gist.github.com/runarorama/a8fab38e473fafa0921d
    - https://twitter.com/runarorama/status/479217956485091328

--- httpz/src/main/scala/Core.scala
+++ httpz/src/main/scala/Core.scala
@@ -1,12 +1,24 @@
 package httpz
 
-import scalaz._
+import scalaz._, Free.FreeC
 import argonaut._
 
-object Core {
+object Core extends Core[RequestF] {
+  /**
+   * @see [[https://dl.dropboxusercontent.com/u/4588997/ReasonablyPriced.pdf]]
+   * @see [[https://gist.github.com/runarorama/a8fab38e473fafa0921d]]
+   */
+  implicit def instance[F[_]](implicit I: Inject[RequestF, F]) =
+    new Core[F]
+}
 
-  def json[A](req: Request)(implicit A: DecodeJson[A]): Action[A] =
-    Action(Free.liftFC(RequestF.one[Error \/ A, Error \/ Json](
+sealed class Core[F[_]](implicit I: Inject[RequestF, F]) {
+
+  private[this] def lift[A, B](f: RequestF[A \/ B]) =
+    EitherT[({type l[a] = FreeC[F, a]})#l, A, B](Free.liftFC(I.inj(f)))
+
+  def json[A](req: Request)(implicit A: DecodeJson[A]): EitherT[({type l[a] = FreeC[F, a]})#l, Error, A] =
+    lift(RequestF.one[Error \/ A, Error \/ Json](
       req,
       \/.left,
       (request, result) => Parse.parse(result).leftMap(Error.parse),
@@ -16,15 +28,15 @@ object Core {
            case -\/((msg, history)) => -\/(Error.decode(request, msg, history, json))
         }
       }
-    )))
+    ))
 
-  def string(req: Request): ActionE[Throwable, String] =
-    Action(Free.liftFC(RequestF.one[Throwable \/ String, String](
+  def string(req: Request): EitherT[({type l[a] = FreeC[F, a]})#l, Throwable, String] =
+    lift(RequestF.one[Throwable \/ String, String](
       req,
       \/.left,
       (_, result) => result,
       (_, result) => \/-(result)
-   )))
+    ))
 
 }
 
