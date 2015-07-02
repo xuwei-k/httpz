76c3dff721f280360a3eb2d284f7ef5ec24a1eb2
xuwei-k <6b656e6a69@gmail.com>
2015-07-02 11:56:50 +0900

    async-http-client 1.9.29

--- project/Build.scala
+++ project/Build.scala
@@ -45,7 +45,7 @@ object build extends Build {
     buildInfoPackage := "httpz.async",
     buildInfoObject := "BuildInfoHttpzAsync",
     libraryDependencies ++= Seq(
-      "com.ning" % "async-http-client" % "1.9.24"
+      "com.ning" % "async-http-client" % "1.9.29"
     )
   ).dependsOn(httpz, tests % "test")
 
