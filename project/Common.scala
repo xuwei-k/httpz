6062ce05b89aa8d6b8abc137b2ee7c5194822655
xuwei-k <6b656e6a69@gmail.com>
2016-11-11 13:28:54 +0900

    scalaz 7.2.7

--- project/Common.scala
+++ project/Common.scala
@@ -13,7 +13,7 @@ object Common {
     sys.process.Process("git rev-parse HEAD").lines_!.head
   ).toOption
 
-  def ScalazVersion = "7.2.6"
+  def ScalazVersion = "7.2.7"
 
   private[this] val unusedWarnings = (
     "-Ywarn-unused" ::
