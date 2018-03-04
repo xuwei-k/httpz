55b7ed0caf8b787abeccce4789869126a6832bbf
kenji yoshida <6b656e6a69@gmail.com>
2018-03-04 11:52:23 +0900

    scalaz 7.2.20

--- project/Common.scala
+++ project/Common.scala
@@ -12,7 +12,7 @@ object Common {
     sys.process.Process("git rev-parse HEAD").lines_!.head
   ).toOption
 
-  def ScalazVersion = "7.2.15"
+  def ScalazVersion = "7.2.20"
 
   private[this] val unusedWarnings = (
     "-Ywarn-unused" ::
