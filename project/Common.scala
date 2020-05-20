091eebf1cb59f00fb4bdefe6f4cfa6b477789a4f
xuwei-k <6b656e6a69@gmail.com>
2020-05-20 01:24:41 +0000

    Update scalaz-core to 7.3.1

--- project/Common.scala
+++ project/Common.scala
@@ -15,7 +15,7 @@ object Common {
       )
       .toOption
 
-  def ScalazVersion = "7.3.0"
+  def ScalazVersion = "7.3.1"
 
   private[this] val unusedWarnings = Def.setting(
     Seq("-Ywarn-unused:imports")
