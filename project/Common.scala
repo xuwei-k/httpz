b13e6b9707a5c5f351237204ea77c644c1ac4617
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2021-08-21 05:12:43 +0000

    Update scalaz-core to 7.3.5

--- project/Common.scala
+++ project/Common.scala
@@ -15,7 +15,7 @@ object Common {
       )
       .toOption
 
-  def ScalazVersion = "7.3.4"
+  def ScalazVersion = "7.3.5"
 
   private[this] val unusedWarnings = Def.setting(
     Seq("-Ywarn-unused:imports")
