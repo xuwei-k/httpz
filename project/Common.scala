c831c52c85d7fc7704970ebd7b16f39ec9851697
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2022-09-14 19:21:12 +0000

    Update scala-library to 2.12.17

--- project/Common.scala
+++ project/Common.scala
@@ -21,7 +21,7 @@ object Common {
     Seq("-Ywarn-unused:imports")
   )
 
-  private[this] val Scala212 = "2.12.16"
+  private[this] val Scala212 = "2.12.17"
 
   val baseSettings = Seq(
     fullResolvers ~= { _.filterNot(_.name == "jcenter") },
